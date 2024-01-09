5db4c4fa667a869157d6b947d97bfeca22c40427
cynthiachinenye <cynthiachiagu@gmail.com>
2024-01-09 18:51:44 +0100

    new

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -6,17 +6,17 @@ import "../Styles/navbar.css"
 
 const Header = () => {
   return (
-    <Navbar expand="lg" className="container-fluid" fixed="top">
+    <Navbar expand="lg" className="container-fluid navbar" fixed="top">
     <Container >
-      <Navbar.Brand href="#home" className='link text-light'>Cyndi...</Navbar.Brand>
-      <Navbar.Toggle className="bg-light" aria-controls="basic-navbar-nav" />
+      <Navbar.Brand href="#home" className='link '>Cyndi...</Navbar.Brand>
+      <Navbar.Toggle className="bg-warning" aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="me-auto text-light">
-          <Nav.Link href="#home" className='text-light auto'>Home</Nav.Link>
-          <Nav.Link href="#about" className='text-light auto'>About Me</Nav.Link>
-          <Nav.Link href="#skill" className='text-light auto'>Skills</Nav.Link>
-          <Nav.Link href="#project" className='text-light auto'>Project</Nav.Link>
-          <Nav.Link href="#contact" className='text-light auto'>Contact</Nav.Link>
+          <Nav.Link href="#home" className=' auto'>Home</Nav.Link>
+          <Nav.Link href="#about" className=' auto'>About Me</Nav.Link>
+          <Nav.Link href="#skill" className=' auto'>Skills</Nav.Link>
+          <Nav.Link href="#project" className=' auto'>Project</Nav.Link>
+          <Nav.Link href="#contact" className=' auto'>Contact</Nav.Link>
           
         </Nav>
       </Navbar.Collapse>
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
+import "../Styles/home.css"
+import {Container} from 'react-bootstrap'
+
 
 const Home = () => {
   return (
-    <div className='home'>
+    <Container className='home '>
     <div>
+    <h3>hi, my name is <span>C</span>ynthia...</h3>
+    <p>I am a Frontend Developer</p>
     </div>
     <img src="" alt="myself"/>
       
-    </div>
+    </Container>
   )
 }
 
