be456f9a097bc14d9fa5e198b6c29ed191a51616
cynthiachinenye <cynthiachiagu@gmail.com>
2024-01-09 14:25:04 +0100

    new

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import "./App.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import {BrowserRouter, Route, Routes} from "react-router-dom"
+import Navbar from "./Components/Navbar";
 function App() {
   return (
-    <div className="App">
+    <div className="container-fluid">
+    <BrowserRouter>
+    <Navbar/>
+    <Routes>
+    
+    </Routes>
+    </BrowserRouter>
       
     </div>
   );
