babe8f7f66427f3e911ce2b846d95936372c921e
cynthiachinenye <cynthiachiagu@gmail.com>
2024-01-09 14:59:36 +0100

    new

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,17 +2,21 @@ import React from 'react'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import "../Styles/navbar.css"
 
 const Header = () => {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
-    <Container>
-      <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar expand="lg" className="container-fluid">
+    <Container >
+      <Navbar.Brand href="#home" className='link text-light'>Cyndi...</Navbar.Brand>
+      <Navbar.Toggle className="bg-light" aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="me-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#link">Link</Nav.Link>
+        <Nav className="me-auto text-light">
+          <Nav.Link href="#home" className='text-light '>Home</Nav.Link>
+          <Nav.Link href="#link" className='text-light '>About Me</Nav.Link>
+          <Nav.Link href="#link" className='text-light '>Skills</Nav.Link>
+          <Nav.Link href="#link" className='text-light '>Project</Nav.Link>
+          <Nav.Link href="#link" className='text-light '>Contact</Nav.Link>
           
         </Nav>
       </Navbar.Collapse>
