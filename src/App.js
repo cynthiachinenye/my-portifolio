9879619c6a5844d401f1d1a8b13809d6c7df6409
cynthiachinenye <cynthiachiagu@gmail.com>
2024-01-09 17:32:05 +0100

    new

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import "./App.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import{Container} from "react-bootstrap"
 import Navbar from "./Components/Navbar";
+import Home from "./Pages/Home";
 function App() {
   return (
     <div className="container-fluid ">
     <Navbar/>
     <Container>
+    <Home/>
     
     </Container>
    
